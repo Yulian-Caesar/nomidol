f000f2ef84102535f9802edc6511f27aa950b4c4
Vladislad T <v.trundenko@everad.com>
2019-05-17 18:31:07 +0300

    vlad

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,38 @@
 
 //=include ../components/footer/footer.js
+
+(function ($) {
+	$(window).on("load", function () {
+			$(".doctor-box").mCustomScrollbar();
+	});
+})(jQuery);
+
+$('.reviews-content').slick({
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 3,
+	arrows: true,
+	responsive: [
+	{
+		breakpoint: 1024,
+		settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2
+		}
+	},
+	{
+		breakpoint: 767,
+		settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2
+		}
+	},
+	{
+		breakpoint: 640,
+		settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1
+		}
+	}
+]
+});
